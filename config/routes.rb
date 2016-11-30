4b63f61f885509794a2549f172f77339f85b420f
Dara Nguyen <dara.nguyenthuy@gmail.com>
2016-11-30 00:17:39 -0800

    Tagging feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Blogger::Application.routes.draw do
 
   # the "resources" tells Rails that we have a resource named "articles" and the router should expect the request to follow RESTful (REpresentational State Transfer) model of the web interaction. For ex, when you make a request like http://localhost:3000/articles/, the router will understand you’re looking for a listing of the articles, and http://localhost:3000/articles/new means you’re trying to create a new article.
 
+  # this is to set the starter page at http://localhost:3000/
+  root to: 'articles#index'
   # if we comment out this line, the Terminal will say: "You don't have any routes defined!""
   resources :articles do
     resources :comments
   end
-  # this is to set the starter page at http://localhost:3000/
-  root to: 'articles#index'
+  resources :tags
+
 end
